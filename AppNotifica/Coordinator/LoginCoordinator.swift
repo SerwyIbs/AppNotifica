826e83d921a722023abb80dbd794ddb6422cdf43
SerwyIbs <117363098+SerwyIbs@users.noreply.github.com>
2022-11-08 11:30:06 -0300

    Criação da estrutura da tela inicial
    Altercação no SceneDelegate para utilizar a estrutura dps Coordinators e Controllers

---
+++ AppNotifica/Coordinator/LoginCoordinator.swift
@@ -0,0 +1,19 @@
+//
+//  LoginCoordinator.swift
+//  AppNotifica
+//
+//  Created by IFB BIOTIC 18 on 08/11/22.
+//
+
+import Foundation
+import UIKit
+class LoginCoordinator: Coordinator {
+    var navigationController: UINavigationController
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    func start(){
+        let viewController = LoginViewController()
+        self.navigationController.pushViewController (viewController, animated: true)
+    }
+}
