85b2846466cba8493a87b72db25acc86820ffbfc
SerwyIbs <117363098+SerwyIbs@users.noreply.github.com>
2022-11-22 10:36:37 -0300

    Video-5Nn

--- AppNotifica/Source/Controller/Register/RegisterView.swift
+++ AppNotifica/Source/Controller/Register/RegisterView.swift
@@ -14,22 +14,14 @@ class RegisterView : UIView {
         self.backgroundColor = .viewBackGroundColor
         setupVisualElements()
     }
-    //criar as funcoes com as propriedades da imagem do login
-    var imageLogin: UIImageView = {
-        let imagem = UIImageView ()
-        imagem.image = UIImage(named:"ImageLogin")
-        imagem.contentMode = .scaleAspectFit
-        imagem.translatesAutoresizingMaskIntoConstraints = false
-        
-        return imagem
-    }()
+
     
     //criar as funcoes com as propriedades da label de login
     var imageLabel: UILabel = {
         let label = UILabel ()
         label.textColor = .textLabelColor
         label.font =  UIFont(name: "SFProDisplay-Light", size: 16)
-        label.text = "Registre e gerencie as ocorrências do seu IF"
+        label.text = "Entre com sei e-mail e sua senha para se registrar"
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
@@ -54,6 +46,15 @@ class RegisterView : UIView {
     return textfFieldS
     }()
     
+    //criar as funcoes com as propriedades da label da senha
+    var confirmesenhaTextfield: UITextField = {
+        let textfFieldS = UITextField ()
+        textfFieldS.font =  UIFont(name: "SFProText-Regular", size: 17)
+        textfFieldS.placeholder = "Confirme sua senha"
+        textfFieldS.backgroundColor = .textFieldBackGroundColor
+        textfFieldS.translatesAutoresizingMaskIntoConstraints = false
+    return textfFieldS
+    }()
     
     //cria a função com as propriadades da butao no logor
     var buttonLogar: UIButton = {
@@ -80,58 +81,60 @@ class RegisterView : UIView {
     
     
     func setupVisualElements(){
-        self.addSubview(imageLogin)
+        
         self.addSubview(imageLabel)
         self.addSubview(emailTexField)
         self.addSubview(senhaTextfield)
         self.addSubview(buttonLogar)
         self.addSubview(buttonRegistrar)
+        self.addSubview(confirmesenhaTextfield)
+        
+    
         
         NSLayoutConstraint.activate([
-            imageLogin.widthAnchor.constraint(equalToConstant: 274.99),
-            imageLogin.heightAnchor.constraint(equalToConstant: 82.64),
-            imageLogin.topAnchor.constraint(equalTo: self.topAnchor,constant: 228),
-            imageLogin.leadingAnchor.constraint(equalTo: self.leadingAnchor,constant: 57),
-            imageLogin.trailingAnchor.constraint(equalTo: self.trailingAnchor,constant: -57),
-        ])
-        
-        NSLayoutConstraint.activate([
-            imageLabel.widthAnchor.constraint(equalToConstant: 700),
-            imageLabel.topAnchor.constraint(equalTo: imageLogin.bottomAnchor,constant: 5),
-            imageLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor,constant: 30),
+            imageLabel.widthAnchor.constraint(equalToConstant: 100),
+            imageLabel.topAnchor.constraint(equalTo: self.topAnchor,constant: 228),
+            imageLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor,constant: 25),
             imageLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor,constant: -10),
         ])
         
         NSLayoutConstraint.activate([
             emailTexField.widthAnchor.constraint(equalToConstant: 374),
-            emailTexField.topAnchor.constraint(equalTo: imageLogin.bottomAnchor,constant: 100),
+            emailTexField.topAnchor.constraint(equalTo: imageLabel.bottomAnchor,constant: 60),
             emailTexField.leadingAnchor.constraint(equalTo: self.leadingAnchor,constant: 30),
             emailTexField.trailingAnchor.constraint(equalTo: self.trailingAnchor,constant: -10),
         ])
         
         NSLayoutConstraint.activate([
             senhaTextfield.widthAnchor.constraint(equalToConstant: 374),
-            senhaTextfield.topAnchor.constraint(equalTo: imageLogin.bottomAnchor,constant: 180),
+            senhaTextfield.topAnchor.constraint(equalTo: emailTexField.bottomAnchor,constant: 180),
             senhaTextfield.leadingAnchor.constraint(equalTo: self.leadingAnchor,constant: 30),
             senhaTextfield.trailingAnchor.constraint(equalTo: self.trailingAnchor,constant: -10),
         ])
         
         NSLayoutConstraint.activate([
-        buttonLogar.widthAnchor.constraint(equalToConstant: 374),
-        buttonLogar.heightAnchor.constraint(equalToConstant: 60),
-        buttonLogar.topAnchor.constraint(equalTo: senhaTextfield.bottomAnchor, constant: 25),
-        buttonLogar.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
-        buttonLogar.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
+            confirmesenhaTextfield.widthAnchor.constraint(equalToConstant: 374),
+            confirmesenhaTextfield.topAnchor.constraint(equalTo: senhaTextfield.bottomAnchor,constant: 180),
+            confirmesenhaTextfield.leadingAnchor.constraint(equalTo: self.leadingAnchor,constant: 30),
+            confirmesenhaTextfield.trailingAnchor.constraint(equalTo: self.trailingAnchor,constant: -10),
         ])
+    
         
         NSLayoutConstraint.activate([
         buttonRegistrar.widthAnchor.constraint(equalToConstant: 374),
         buttonRegistrar.heightAnchor.constraint(equalToConstant: 60),
-        buttonRegistrar.topAnchor.constraint(equalTo: buttonLogar.bottomAnchor, constant: 25),
+        buttonRegistrar.topAnchor.constraint(equalTo: confirmesenhaTextfield.bottomAnchor, constant: 25),
         buttonRegistrar.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
         buttonRegistrar.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
         ])
 
+        NSLayoutConstraint.activate([
+        buttonLogar.widthAnchor.constraint(equalToConstant: 374),
+        buttonLogar.heightAnchor.constraint(equalToConstant: 60),
+        buttonLogar.topAnchor.constraint(equalTo: buttonRegistrar.bottomAnchor, constant: 25),
+        buttonLogar.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+        buttonLogar.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
+        ])
     }
  
     required init?(coder: NSCoder) {
